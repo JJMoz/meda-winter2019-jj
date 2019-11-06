8c00e5ffc198f567965c390b044ccae97860ab13
JJMoz <18526918+JJMoz@users.noreply.github.c>
2019-11-06 09:00:26 -0800

    truck

--- practice/OOP/vehicles/index.js
+++ practice/OOP/vehicles/index.js
@@ -2,10 +2,14 @@
 //// pulling in cas.js and car.js is pulling in vehicle.js. so vhicle.js is pulled into index.js
 const car = require("./truck.js");
 
-let honda = new car("Jesse", "Honda", "Civic", 2000,"white", "8EFD39E9", 125000, 35, 12, 150, "Diesel", 5, 10);
+let honda = new car("Jesse", "Honda", "Civic", 2000,"white", "8EFD39E9", 125000, 35, 12, 150, "Gas", 0, 1);
 
 console.log(honda);
 // honda.travel(100);
 // honda.travel(80);
 // honda.refuel(10);
 honda.travel(50);
+
+let truck = new car ("Bob", "Mac", "Trailer", 2000,"red", "TEFD39E9", 425000, 20, 12, 100, "Diesel", 5, 10);
+
+console.log(truck);
