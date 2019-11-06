c0e721b2013ed90d6c6f49208d3b8769cc5e43ee
JJMoz <18526918+JJMoz@users.noreply.github.c>
2019-11-06 08:47:46 -0800

    vehicle, car , truck

--- practice/OOP/vehicles/car.js
+++ practice/OOP/vehicles/car.js
@@ -11,10 +11,44 @@ const car = class Car extends Vehicle{
     }
     //method called travel. miles travel = (will return) how long it took and how much gas is left
     travel(miles) {
-        this.currentFuel = this.currentFuel - (miles / this.mpg);
-        console.log("The " + this.make + "" + this.model + " has " + this.currentFuel.toFixed(1) + " gallons of " + this.engineType + " left. ");
-    }
+       if (this.currentFuel >= 0) {
+           if (this.currentFuel * this.mpg >= miles) {
+                this.currentFuel = this.currentFuel - (miles / this.mpg);
+                console.log("The " + this.make + " " + this.model + " has " + this.currentFuel.toFixed(1) + " gallons of " + this.engineType + " left. ");
+                } else { 
+                    console.log("The " + this.make + " " + this.model + " Does not have enough fuel to travel that far. ");
+                }
+
+                } else {
+                    console.log("The " + this.make + " " + this.model + " has no gas. ");
+                }
+            
+            }
+
+            refuel(gallons){
+                if (gallons <= 0){
+                    console.log("You need to provide fuel (positive number). ");
+                    return 1;
+                } else {
+
+                    if (gallons >= this.tankSize){
+                        console.log("You provided too much fuel, cannot fint into tank.")
+                    }else{
+
+                        if(this.tankSize - this.currentFuel < gallons) {
+                            console.log("there is not enough free space in the fuel tank to add that many gallons.")
+                        } else {
+                            this.currentFuel = this.currentFuel + gallons;
+                            console.log(this.model + " was successfully refueled with" + gallons + "gallons of gas. ");
+                        }
+                    }
+                }
+
+
+            }
+           
+        }
 // .toFixed (1) ---> rounds number to first decimal
-}
+
 
 module.exports = car;
--- practice/OOP/vehicles/index.js
+++ practice/OOP/vehicles/index.js
@@ -1,9 +1,11 @@
 // require pulls in file
 //// pulling in cas.js and car.js is pulling in vehicle.js. so vhicle.js is pulled into index.js
-const car = require("./car.js");
+const car = require("./truck.js");
 
-let honda = new car("Jesse", "Honda", "Civic", 2000,"white", "8EFD39E9", 125000, 35, 12, 150, "Gas");
+let honda = new car("Jesse", "Honda", "Civic", 2000,"white", "8EFD39E9", 125000, 35, 12, 150, "Diesel", 5, 10);
 
 console.log(honda);
-honda.travel(100);
-honda.travel(80);
+// honda.travel(100);
+// honda.travel(80);
+// honda.refuel(10);
+honda.travel(50);
