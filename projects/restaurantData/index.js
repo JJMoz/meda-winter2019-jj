94893c5ee39a19045a71e661d7a60774c26500cb
JJMoz <18526918+JJMoz@users.noreply.github.c>
2019-12-04 13:06:44 -0800

    restaurant data

---
+++ projects/restaurantData/index.js
@@ -0,0 +1,6 @@
+//RESTUARANT DATASET
+const fs = require("fs");
+const _ = require("lodash");
+
+
+const dataset = fs.readFileSync("restaurantScores.csv", "utf8");
---
+++ projects/restaurantData/restDataQuestions.js
@@ -0,0 +1,7 @@
+// RESTAURANT QUESTIONS
+// Average Health Score?
+// Most common violation?
+// Who had the lowest Score?
+// Average score of unscheduled inspections?
+// Average score of scheduled(follow-up) Reinspections?
+
