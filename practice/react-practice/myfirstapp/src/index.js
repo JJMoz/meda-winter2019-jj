df96dca948d9713073e7fcb0f803b39ed4490459
JJMoz <18526918+JJMoz@users.noreply.github.c>
2019-12-10 13:26:06 -0800

    React intro

--- practice/react-practice/myfirstapp/src/index.js
+++ practice/react-practice/myfirstapp/src/index.js
@@ -1,4 +1,4 @@
-// import is like require but import lets you pick certain packages (parts) of the library
+// import is like require but import lets you pick certain packages (parts) of the library. Installs React Developer Tools for Browser
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -19,12 +19,54 @@ const paragraph = React.createElement(
     null,
     "this is a paragraph"
 );
+//JS function. need to capitalize, to differentatate between html...creates reusablity.
+function About (){
+    return (
+        <p>This is officially a React Function Component</p>
+    );
+}
+// most use "props" for React, props= properties
+function H1Generator(props){
+
+       let temp = props.text;
+        
+       return (
+           <h1 className="myCSSClass">{props.text}</h1> 
+    );
+}
+// below is better practice to do above, arrow function
+const H2Generator = (props) => {
+    return (<h2>{props.something}</h2>);
+}
+
+// class extends another class, it inheritence..
+class Counter extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            score: 0
+        };
+    }
+
+    render() {
+        return (<p>The current score is:{this.props.temp} </p>);
+    }
+
+}
+
 
 const idOfDiv = "test";
-// JSX version of createElemnet();
+// JSX version of createElemnet(); same as above
 const app = (
-    // To "interpolate" JavaScript, use curly braces.
+    // To "interpolate" JavaScript, use curly braces. (insert something of a different nature into something else )// xml needs a space before closing bracket(space / >)
     <div id={idOfDiv}>
+    <about tempScore={3} />
+
+    <H1Generator text="Hi this custom text" />
+    <H2Generator something="This is H2 line " />
+    <H1Generator text= "This is some other line." />
+    <Counter />
         {title}
         {paragraph}
     </div>
@@ -32,8 +74,16 @@ const app = (
 
 
 
-//render tells it to put it in the webpage... Render component to target element.
+//render tells it to put it in the webpage... Render component to target element.(2 arguments)
 ReactDOM.render(
     app,
+    // root is in the index.html, it is auto created.
     document.getElementById("root")
 );
+
+
+// JSX SYNTAX
+// Anything that is written in JSX should go in between a pair of parentthese.
+// Any Javascript inside of JSX should be written between a pair of curly braces
+// EXAMPLE
+//     let text = (<p>{someJSVariable}</p>);
